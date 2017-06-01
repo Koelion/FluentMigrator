9ccfbf7bf20402d3816f2c54752dd60817e0c5a8
Bartek <koelionekpl@gmail.com>
2017-06-01 16:10:57 +0200

    Add project files.

---
+++ FluentMigrator/Migrations.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentMigrator.Migrations
+{
+
+
+    [Migration(1)]
+    public class TestTable_1 : Migration
+    {
+        public override void Down()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            Execute.Script(@"Assets\createtesttable.txt");
+        }
+
+    }
+
+    [Migration(2)]
+    public class TestTable_2 : Migration
+    {
+        public override void Down()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            string sql = "ALTER TABLE [dbo].[TestTable] ALTER COLUMN [PostCode] varchar(8); " +
+                         "ALTER TABLE [dbo].[TestTable] ALTER COLUMN [Phone] varchar(16); ";
+            Execute.Sql(sql);
+            //Alter.Table("[dbo].[TestTable]").AlterColumn("[PostCode]").AsFixedLengthString(8);
+            //Alter.Table("[dbo].[TestTable]").AlterColumn("[Phone]").AsFixedLengthString(16);
+        }
+    }
+
+    [Migration(3)]
+    public class TestTable_3 : Migration
+    {
+        public override void Down()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            Alter.Table("[TestTable]").AddColumn("[IsDeleted]").AsByte();
+        }
+
+    }
+}
---
+++ FluentMigrator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using FluentMigrator;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+using FluentMigrator.Runner.Processors;
+using FluentMigrator.Runner.Processors.SqlServer;
+
+namespace FluentMigrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=localhost;Initial Catalog=test;Integrated Security=True";
+            Announcer announcer = new TextWriterAnnouncer(s => System.Diagnostics.Debug.WriteLine(s));
+            announcer.ShowSql = true;
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            IRunnerContext migrationContext = new RunnerContext(announcer);
+
+            var options = new ProcessorOptions
+            {
+                PreviewOnly = false,
+                Timeout = 60
+            };
+            var factory = new SqlServer2014ProcessorFactory();
+            using (IMigrationProcessor processor = factory.Create(connectionString, announcer, options))
+            {
+                var runner = new MigrationRunner(assembly, migrationContext, processor);
+                runner.MigrateUp(true);
+
+            }
+        }
+    
+    }
+}
